bf975d522ca8eac4ef1034d99503ce5560cd359f
Stefan Mertl <info@mertl-research.at>
2019-08-16 15:38:40 +0200

    Proper marker sizes and colors. Set marker size to a fixed pixel width.

--- src/components/PGVLegend.vue
+++ src/components/PGVLegend.vue
@@ -3,10 +3,11 @@
         <circle v-for="(cur_pgv, k) in pgv_values"
                 v-bind:key="cur_pgv"
                 :id="'pgv_legend_marker_' + k"
-                :cx="k * 100"
-                cy="100"
-                r="10"
-                fill="black"/>
+                :pgv="pgv_values[k]"
+                :r="pgv_radius[k]"
+                :cx="marker_position[k].x"
+                :cy="marker_position[k].y"
+                :fill="marker_color[k]"/>
     </g>
 </template>
 
@@ -20,28 +21,93 @@ export default {
     props: {
         name: String,
         pgv_values: Array,
+        radius_limits: Array,
+        pgv_limits: Array,
+        map_limits: Object,
+        position: Object,
     },
 
     mounted () {
-        var markers = d3.selectAll("circle[id^='pgv_legend_marker']");
-        var self = this;
+        //var markers = d3.selectAll("circle[id^='pgv_legend_marker']");
+        //var self = this;
+        var map_svg = d3.select("#map");
+
+        this.svg_matrix = map_svg.node().getScreenCTM();
+
+        /*
         markers.each(function(d, k) {
             console.log("Processing marker " + k +".", this);
             var cur_el = d3.select(this);
-            cur_el.attr("r", self.pgv_radius(self.pgv_values[k]));
-            //cur_el.attr("r", 30);
+            cur_el.attr("cx", self.$props.position.x + k * 200)
+                  .attr("cy", self.$props.position.y);
+
         });
+        */
+
+        for (var k = 0; k < this.pgv_values.length; k++)
+        {
+            this.marker_color[k] = this.pgv_to_color(this.pgv_values[k]);
+        }
+
+        window.addEventListener('resize', this.on_resize);
     },
 
     data() {
         return {
-            scale: 2,
+            scale: 1,
+            svg_matrix: [],
+            marker_color: [],
         };
     },
 
     computed: {
         element_id: function() {
             return 'pgvlegend_' + this.name;
+        },
+
+        svg_scale: function() {
+            var scale = 1;
+            if (this.svg_matrix.a)
+            {
+                scale = this.svg_matrix.a
+            }
+            return scale;
+        },
+
+        pgv_radius: function() {
+            var radius = [];
+
+            for (var k = 0; k < this.pgv_values.length; k++)
+            {
+                radius.push(this.compute_pgv_radius(this.pgv_values[k]));
+            }
+            return radius;
+        },
+
+        marker_position: function() {
+            var position = [];
+            var gap_x = 10;
+
+            for (var k = 0; k < this.pgv_values.length; k++)
+            {
+                var cur_pos = {x: 0, y: 0};
+                var space = 0;
+                if (k == 0)
+                {
+                    space = this.pgv_radius[k];
+                    cur_pos.x = this.$props.position.x + space + gap_x;
+                    cur_pos.y = this.$props.position.y;
+                }
+                else
+                {
+                    space = this.pgv_radius[k-1] + this.pgv_radius[k];
+                    cur_pos.x = position[k-1].x + space + gap_x;
+                    cur_pos.y = this.$props.position.y;
+                }
+                position.push(cur_pos);
+            }
+
+            return position;
         },
 
     },
@@ -54,28 +120,23 @@ export default {
             const y = d3.scaleLinear().domain([this.map_limits.y_min,
                                                this.map_limits.y_max])
                                        .range([2500, 0]);
-            return {x, y};
+            const radius = d3.scaleLog().domain(this.$props.pgv_limits)
+                                        .range(this.$props.radius_limits);
+            return {x, y, radius};
         },
 
-        pgv_radius: function(pgv) {
+        compute_pgv_radius: function(pgv) {
             var cur_pgv = pgv;
-            var map_svg = d3.select("#map");
-            var matrix = map_svg.node().getScreenCTM();
-            console.log("matrix: ", matrix);
-            console.log("pgv: ", cur_pgv);
+            var radius = 4;
 
             if (cur_pgv) {
-                var radius = Math.log10(cur_pgv * 1000) + 6;
-                if (radius > 20)
-                {   
-                    radius = 20;
-                }
-                return this.scale * radius / matrix.a;
+                const scales = this.get_scales();
+                radius = this.scale * scales.radius(cur_pgv);
             }
-            else {
-                return 4 / matrix.a;
-            }
+
+            return radius / this.svg_scale;
         },
+
 
         pgv_to_color(pgv) {
             // Convert the PGV value [m/s] to a color value.
@@ -85,6 +146,17 @@ export default {
 
             return color;
         },
+
+        on_resize: function() {
+            //var markers = d3.selectAll("circle[id^='pgv_legend_marker']");
+            //var self = this;
+            //markers.each(function(d, k) {
+            //    var cur_el = d3.select(this);
+            //    cur_el.attr("r", self.pgv_radius(self.pgv_values[k]));
+            //});
+            var map_svg = d3.select("#map");
+            this.svg_matrix = map_svg.node().getScreenCTM();
+        }
     },
 }
 </script>
