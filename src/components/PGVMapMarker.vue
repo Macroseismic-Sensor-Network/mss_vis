59276c68fab1b1bee1fc638e6fbee01877295b65
Stefan Mertl <info@mertl-research.at>
2019-08-16 10:38:35 +0200

    Set the size of the markers to a fixed pixel value.

--- src/components/PGVMapMarker.vue
+++ src/components/PGVMapMarker.vue
@@ -3,13 +3,14 @@
         <circle v-bind:id="element_id + '_current'"
                 :r="pgv_radius"
                 :fill="pgv_fill"
-                :stroke="pgv_stroke"/>
+                :stroke="pgv_stroke"
+                :fill-opacity="current_fill_opacity"/>
 
         <circle v-bind:id="element_id + '_max'"
                 :r="pgv_max_radius"
                 :fill="pgv_max_fill"
                 :stroke="pgv_max_stroke"
-                fill-opacity="0.4"/>
+                :fill-opacity="max_fill_opacity"/>
     </g>
 </template>
 
@@ -25,6 +26,7 @@ export default {
         x_utm: String,
         y_utm: String,
         map_limits: Object,
+        map_size: Object,
     },
 
     mounted () {
@@ -44,7 +46,9 @@ export default {
     data() {
         return {
             radius: 4,
-            scale: 2,
+            scale: 5,
+            current_fill_opacity: 0.5,
+            max_fill_opacity: 0.3,
         };
     },
 
@@ -55,7 +59,8 @@ export default {
 
         pgv_radius: function() {
             var cur_pgv = this.$store.getters.current_pgv_by_station(this.$props.station_id);
-
+            var map_svg = d3.select("#map");
+            var matrix = map_svg.node().getScreenCTM();
 
             if (cur_pgv) {
                 var radius = Math.log10(cur_pgv * 1000) + 6;
@@ -63,15 +68,17 @@ export default {
                 {   
                     radius = 20;
                 }
-                return this.scale * radius;
+                return this.scale * radius / matrix.a;
             }
             else {
-                return 4;
+                return 4 / matrix.a;
             }
         },
 
         pgv_max_radius: function() {
             var cur_pgv = this.$store.getters.disp_range_max_pgv_by_station(this.$props.station_id);
+            var map_svg = d3.select("#map");
+            var matrix = map_svg.node().getScreenCTM();
 
             if (cur_pgv) {
                 var radius = Math.log10(cur_pgv * 1000) + 6;
@@ -79,7 +86,7 @@ export default {
                 {   
                     radius = 20;
                 }
-                return this.scale * radius;
+                return this.scale * radius / matrix.a;
             }
             else {
                 return 0;
@@ -145,10 +152,10 @@ export default {
         get_scales() {
             const x = d3.scaleLinear().domain([this.map_limits.x_min, 
                                                this.map_limits.x_max])
-                                       .range([0, 1280]);
+                                       .range([0, 4000]);
             const y = d3.scaleLinear().domain([this.map_limits.y_min,
                                                this.map_limits.y_max])
-                                       .range([720, 0]);
+                                       .range([2500, 0]);
             return {x, y};
         },
 
