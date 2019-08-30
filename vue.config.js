d10d825c18f8c0db7e0aa41d67009e7cfab96812
Stefan Mertl <info@mertl-research.at>
2019-08-30 16:11:29 +0200

    Use the html-webpack-plugin to create the css and javascript templates for jekyll. Write all assets to assets/vue.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,40 @@
 // Vue configuration
+//const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     publicPath: "./",
+    assetsDir: "assets/vue",
     configureWebpack: {
+
         resolve: {
             alias: {
                 vue$: 'vue/dist/vue.esm.js'
             },
             extensions: ['*', '.js', '.vue', '.json']
         },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: '_includes/vue/vue_scripts.html',
+                template: 'src/templates/scripts.html',
+                inject: false,
+            }),
+            new HtmlWebpackPlugin({
+                filename: '_includes/vue/vue_css.html',
+                template: 'src/templates/css.html',
+                inject: false,
+            }),
+        ],
     },
+
+    /*
+    chainWebpack: webpackConfig => {
+        if (process.env.NODE_ENV === 'production') {
+            const assets_path = 'assets/vue';
+            webpackConfig
+                .output
+                .filename(path.join(assets_path, 'js/[name].[chunkhash:8].js'))
+                .chunkFilename(path.join(assets_path, 'js/chunk[id].[chunkhash:8].js'))
+        }
+    },*/
 };
