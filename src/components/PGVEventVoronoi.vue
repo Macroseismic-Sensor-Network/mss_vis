ff6e7b75b4f49276b98a09753200b2166a14a085
Stefan Mertl <info@mertl-research.at>
2019-11-04 18:36:27 +0100

    Added a first draft of the display of the event detection result. This outlines the way to watch for event detection result changes and update the Voronoi plot accordingly.

--- src/components/PGVEventVoronoi.vue
+++ src/components/PGVEventVoronoi.vue
@@ -15,47 +15,6 @@ export default {
     },
 
     mounted () {
-        // Create the Delaunay triangles.
-        var vertices = [];
-        var scales= this.scales;
-        console.log(this.stations);
-        for (const cur_station of this.stations) { 
-            vertices.push([cur_station.x_utm, cur_station.y_utm])
-        }
-        console.log(vertices);
-
-        console.log(this.map_limits);
-        var voronoi = d3.voronoi();
-        voronoi.extent([[this.map_limits.x_min, this.map_limits.y_min],
-                       [this.map_limits.x_max, this.map_limits.y_max]]);
-        var triangles = voronoi.triangles(vertices);
-        var polygons = voronoi.polygons(vertices);
-
-        var line_generator = d3.line().x(function(d) { return scales.x(d[0]); })
-                                      .y(function(d) { return scales.y(d[1]); });
-
-        var container = d3.select('#' + this.event_id);
-        //container.append('path');
-
-        var closed_poly;
-        for (const cur_poly of triangles) {
-            closed_poly = cur_poly;
-            closed_poly.push(cur_poly[0]);
-            container.append('path').attr('d', line_generator(closed_poly))
-                                    .attr('stroke', 'orange')
-                                    .attr('stroke-width', 5)
-                                    .attr('fill', 'none');
-        }
-
-        for (const cur_poly of polygons) {
-            closed_poly = cur_poly;
-            closed_poly.push(cur_poly[0]);
-            container.append('path').attr('d', line_generator(closed_poly))
-                                    .attr('stroke', 'black')
-                                    .attr('stroke-width', 5)
-                                    .attr('fill', 'none');
-        }
-
     },
 
     data() {
@@ -76,9 +35,74 @@ export default {
             return this.$store.getters.map_config.map_limits;
         },
 
+        detection_result: function() {
+            return this.$store.getters.detection_result;
+        },
+    },
+
+    watch: {
+        detection_result(new_value, old_value) {
+            console.log('Detection result has changed.' + new_value + old_value);
+            this.plot_detection_result();
+        },
     },
 
     methods: {
+        plot_detection_result() {
+            console.log("Plotting the detection result.");
+
+            // Create the Delaunay triangles.
+            var vertices = [];
+            var scales= this.scales;
+            var used_stations = this.detection_result.used_stations;
+            var stations = [];
+
+            for (const cur_snl of used_stations) {
+                for (const cur_station of this.stations) {
+                    if (cur_station.name == cur_snl[0]) {
+                        stations.push(cur_station);
+                        break;
+                    }
+                }
+            }
+            console.log('Stations: ', stations);
+            for (const cur_station of stations) { 
+                vertices.push([cur_station.x_utm, cur_station.y_utm])
+            }
+            console.log(vertices);
+
+            console.log(this.map_limits);
+            var voronoi = d3.voronoi();
+            voronoi.extent([[this.map_limits.x_min, this.map_limits.y_min],
+                           [this.map_limits.x_max, this.map_limits.y_max]]);
+            var triangles = voronoi.triangles(vertices);
+            var polygons = voronoi.polygons(vertices);
+
+            var line_generator = d3.line().x(function(d) { return scales.x(d[0]); })
+                                          .y(function(d) { return scales.y(d[1]); });
+
+            var container = d3.select('#' + this.event_id);
+            //container.append('path');
+
+            var closed_poly;
+            for (const cur_poly of triangles) {
+                closed_poly = cur_poly;
+                closed_poly.push(cur_poly[0]);
+                container.append('path').attr('d', line_generator(closed_poly))
+                                        .attr('stroke', 'orange')
+                                        .attr('stroke-width', 5)
+                                        .attr('fill', 'none');
+            }
+
+            for (const cur_poly of polygons) {
+                closed_poly = cur_poly;
+                closed_poly.push(cur_poly[0]);
+                container.append('path').attr('d', line_generator(closed_poly))
+                                        .attr('stroke', 'black')
+                                        .attr('stroke-width', 5)
+                                        .attr('fill', 'none');
+            }
+        },
     },
 }
 </script>
