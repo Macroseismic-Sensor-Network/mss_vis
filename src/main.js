b027eacd7dd35b03d78ac245723fa30bce722b27
Stefan Mertl <info@mertl-research.at>
2019-12-05 17:07:08 +0100

    Register the mss-display component.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import Vue from 'vue'
 import PGVMap from './components/PGVMap.vue'
+import MSSDisplay from './components/MSSDisplay.vue'
 import store from './store/store.js'
 import VueNativeSock from 'vue-native-websocket'
 
 Vue.config.productionTip = false
 
 Vue.use(VueNativeSock,
-        'ws://mss.mertl-research.at:8100', 
-        //'ws://localhost:8100', 
+        //'ws://mss.mertl-research.at:8100', 
+        'ws://localhost:8100', 
         {store: store,
          format: 'json',
          reconnection: true,
          reconnectionDelay: 3000});
 
+
 Vue.component('pgv-map', PGVMap);
+Vue.component('mss-display', MSSDisplay);
 
 new Vue({
     store,
