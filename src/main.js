6c3f17b024e19d3befdc101a7beb9e15c5a6ca9e
Stefan Mertl <info@stefanmertl.com>
2019-12-10 17:25:03 +0100

    Register the timeseries display component.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import PGVMap from './components/PGVMap.vue'
 import MSSDisplay from './components/MSSDisplay.vue'
+import MSSTimeSeriesDisplay from './components/MSSTimeSeriesDisplay.vue'
 import store from './store/store.js'
 import VueNativeSock from 'vue-native-websocket'
 
 Vue.config.productionTip = false
 
 Vue.use(VueNativeSock,
-        //'ws://mss.mertl-research.at:8100', 
-        'ws://localhost:8100', 
+        'ws://mss.mertl-research.at:8100', 
+        //'ws://localhost:8100', 
         {store: store,
          format: 'json',
          reconnection: true,
@@ -17,6 +18,7 @@ Vue.use(VueNativeSock,
 
 Vue.component('pgv-map', PGVMap);
 Vue.component('mss-display', MSSDisplay);
+Vue.component('mss-time-series-display', MSSTimeSeriesDisplay);
 
 new Vue({
     store,
