94ed549c712966a570749247015eb91a6576507d
Stefan Mertl <info@mertl-research.at>
2019-12-10 19:56:54 +0100

    Removed the timeseries display.

--- src/components/MSSDisplay.vue
+++ src/components/MSSDisplay.vue
@@ -3,22 +3,12 @@
         <div id="map-container" class = "grid-x">
             <div class="cell"><PGVMap /></div>
         </div>
-        <!--
-        <div id="timeseries-container" class = "grid-x">
-            <div class="cell"><PGVGraph v-for="cur_station in stations"
-                                        v-bind:key="cur_station.id"
-                                        v-bind:stream_id="cur_station.id" 
-                                        height=200 />
-            </div>
-        </div>
-        -->
     </div>
 </template>
 
 <script>
 
 import PGVMap from '../components/PGVMap.vue'
-import PGVGraph from '../components/PGVGraph.vue'
 
 export default {
     name: 'MSSDisplay',
@@ -27,8 +17,6 @@ export default {
     },
     components: {
         PGVMap,
-        // eslint-disable-next-line
-        PGVGraph,
     },
     computed: {
         stations: function() {
