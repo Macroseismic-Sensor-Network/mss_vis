404b927e369776e51d5380524a77b6b3aeb8da92
Stefan Mertl <info@mertl-research.at>
2019-12-16 19:38:01 +0100

    Added the license information.

--- src/components/ArchiveEvent.vue
+++ src/components/ArchiveEvent.vue
@@ -1,3 +1,29 @@
+<!--
+/*****************************************************************************
+ * LICENSE
+ *
+ * This file is part of mss_vis.
+ * 
+ * If you use mss_vis in any program or publication, please inform and
+ * acknowledge its authors.
+ * 
+ * mss_vis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * mss_vis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with mss_vis. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2019 Stefan Mertl
+ *****************************************************************************/
+-->
+
 <template>
     <div class="archive_event" v-on:click="show_event"><span class="archive_event" v-bind:class="{ active: is_active }" v-bind:style="{backgroundColor: color}">{{ event_id }} ({{ (max_pgv * 1000).toFixed(3) }} mm/s)</span><br></div>
 </template>
